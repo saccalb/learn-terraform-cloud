9f135337dd01488c40414d40bea4f17cc20ea165
root <root@ip-172-31-14-206.eu-south-1.compute.internal>
2022-05-02 15:40:41 +0000

    Remove cloud block

--- versions.tf
+++ versions.tf
@@ -1,13 +1,14 @@
 terraform {
 
   cloud {
-    organization = "hashicorp-learn"
+/*
+    organization = "saccalb"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
